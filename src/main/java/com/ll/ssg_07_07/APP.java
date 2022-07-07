3009995f8f11408e27011ad735e0c4d37e81eb0d
gusdn0236 <hdno0236@naver.com>
2022-07-07 17:09:31 +0900

    APP클래스 sc.nextLine으로 수정

--- src/main/java/com/ll/ssg_07_07/APP.java
+++ src/main/java/com/ll/ssg_07_07/APP.java
@@ -16,23 +16,22 @@ public class APP {
         outer:
         while (true) {
             System.out.printf("명령) ");
-            String cmd = sc.next().trim();
+            String cmd = sc.nextLine().trim();
 
             switch (cmd) {
                 case "등록":
                     System.out.printf("명언 : ");
-                    String content = sc.next().trim();
+                    String content = sc.nextLine().trim();
                     System.out.printf("작가 : ");
-                    String author = sc.next().trim();
+                    String author = sc.nextLine().trim();
                     int id = ++wiseSayingLastId;  // 명언 번호 증가
 
-                    WiseSaying WiseSaying = new WiseSaying(id,content,author);
+                    WiseSaying WiseSaying = new WiseSaying(id, content, author);
                     System.out.println(WiseSaying);
 
 
-                    System.out.printf("%d번 명언이 등록되었습니다.\n",id);
+                    System.out.printf("%d번 명언이 등록되었습니다.\n", id);
                     break;
-
 
 
                 case "종료":
