00964101a56b34a4ce5e380dfff6fd7b25a7b470
gusdn0236 <hdno0236@naver.com>
2022-07-11 19:32:45 +0900

    URL 처리기, Rq클래스 기능 구현

---
+++ src/main/java/com/ll/ssg_07_07/Rq.java
@@ -0,0 +1,29 @@
+package com.ll.ssg_07_07;
+
+public class Rq {
+    public String url;
+
+    public Rq(String url) {
+        this.url = url;
+
+
+    }
+
+    public int getIntParam(String paramName, int defaultValue) {
+        //"검색?id=10&no=1"
+        String[] urlBits = url.split("\\?", 2);
+        urlBits = urlBits[1].split("&");
+
+        for (String urlBit : urlBits) {
+            String[] paramNameAndValue = urlBit.split("=", 2);
+            String paramName_ = paramNameAndValue[0];
+            String paramValue = paramNameAndValue[1];
+
+            if (paramName.equals(paramName_)) {
+                return Integer.parseInt(paramValue);
+            }
+        }
+
+        return defaultValue;
+    }
+}
