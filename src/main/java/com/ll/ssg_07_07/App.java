cb308c3b6d518d6a17bd27d9a5ce8cdd4f3f218d
gusdn0236 <hdno0236@naver.com>
2022-07-14 14:28:51 +0900

    Rq에 Map 도입

--- src/main/java/com/ll/ssg_07_07/App.java
+++ src/main/java/com/ll/ssg_07_07/App.java
@@ -5,12 +5,19 @@ import java.util.List;
 import java.util.Scanner;
 
 public class App {
+   private Scanner sc;
+   private List<WiseSaying> WList;
+   private int WiseSayingLastId;
+
+    App(){
+       sc  = new Scanner(System.in);
+       WList = new ArrayList<>();
+       WiseSayingLastId  = 0;
+    }
     public void run() {
-        Scanner sc = new Scanner(System.in);
+
         System.out.println("=== 명언 SSG ===");
 
-        List<WiseSaying> WList = new ArrayList<>();
-        int WiseSayingLastId = 0;
 
         outer:
         while (true) {
@@ -22,30 +29,7 @@ public class App {
             switch (rq.getPath()) {
 // 삭제?id=2
                 case "삭제":
-
-                    int paramId = rq.getIntParam("id", 0);
-
-                    if (paramId == 0) {
-                        System.out.printf("id를 입력해주세요\n");
-                        continue;
-                    }
-
-                    WiseSaying wisedel = null;
-
-                    for (WiseSaying w : WList) {
-                        if (w.id == paramId) {
-                            wisedel = w;
-                        }
-                    }
-                    if (wisedel == null) {
-                        System.out.printf("%d번 명언은 존재하지 않습니다.\n", paramId);
-                        continue;
-                    }
-
-                    WList.remove(wisedel);
-                    System.out.printf("%d번 명언이 삭제되었습니다.\n", paramId);
-
-
+                  remove(rq);
                     break;
                 case "수정":
                     int paramIdmod = rq.getIntParam("id", 0);
@@ -67,10 +51,10 @@ public class App {
                         continue;
                     }
 
-                    System.out.printf("명언(기존) : %s\n",wisemod.content);
+                    System.out.printf("명언(기존) : %s\n", wisemod.content);
                     System.out.printf("명언 : ");
                     wisemod.content = sc.nextLine().trim();
-                    System.out.printf("작가(기존) : %s\n",wisemod.author);
+                    System.out.printf("작가(기존) : %s\n", wisemod.author);
                     System.out.printf("작가 : ");
                     wisemod.author = sc.nextLine().trim();
 
@@ -78,32 +62,11 @@ public class App {
 
                     break;
                 case "목록":
-                    if (WiseSayingLastId == 0) {
-                        System.out.println("명언을 등록해주세요");
-                        break;
-                    }
-                    System.out.println("번호 / 명언 / 작가");
-                    System.out.println("----------------");
-
-
-                    for (int i = WList.size() - 1; i >= 0; i--) {
-                        WiseSaying w = WList.get(i);
-                        System.out.printf("%d / %s / %s\n", w.id, w.content, w.author);
-                    }
-
+                   list(rq);
                     break;
 
                 case "등록":
-                    System.out.printf("명언 : ");
-                    String content = sc.nextLine();
-                    System.out.printf("작가 : ");
-                    String author = sc.nextLine();
-                    int id = ++WiseSayingLastId;
-                    WiseSaying w = new WiseSaying(id, content, author);
-                    WList.add(w);
-
-
-                    System.out.printf("%d번째 명언이 등록되었습니다.\n", id);
+                    write(rq);
                     break;
 
                 case "종료":
@@ -113,5 +76,61 @@ public class App {
 
         }
 
+    }
+
+    private void list(Rq rq) {
+
+        System.out.println("번호 / 명언 / 작가");
+        System.out.println("----------------");
+
+
+        for (int i = WList.size() - 1; i >= 0; i--) {
+            WiseSaying w = WList.get(i);
+            System.out.printf("%d / %s / %s\n", w.id, w.content, w.author);
+        }
+
+    }
+
+    private void remove(Rq rq) {
+        int paramId = rq.getIntParam("id", 0);
+
+        if (paramId == 0) {
+            System.out.printf("id를 입력해주세요\n");
+            return;
+        }
+
+        WiseSaying foundWiseSaying = findById(paramId);
+        if (foundWiseSaying == null) {
+            System.out.printf("%d번 명언은 존재하지 않습니다.\n", paramId);
+            return;
+        }
+
+        WList.remove(foundWiseSaying);
+        System.out.printf("%d번 명언이 삭제되었습니다.\n", paramId);
+
+
+    }
+
+    private WiseSaying findById(int paramId) {
+
+        for (WiseSaying w : WList) {
+            if (w.id == paramId) {
+               return w;
+            }
+        }
+        return null;
+    }
+
+    private void write(Rq rq) {
+        System.out.printf("명언 : ");
+        String content = sc.nextLine();
+        System.out.printf("작가 : ");
+        String author = sc.nextLine();
+        int id = ++WiseSayingLastId;
+        WiseSaying w = new WiseSaying(id, content, author);
+        WList.add(w);
+
+
+        System.out.printf("%d번째 명언이 등록되었습니다.\n", id);
     }
 }
--- src/main/java/com/ll/ssg_07_07/Rq.java
+++ src/main/java/com/ll/ssg_07_07/Rq.java
@@ -1,43 +1,56 @@
 package com.ll.ssg_07_07;
 
+import java.util.HashMap;
+import java.util.Map;
+
 public class Rq {
     String url;
     String path;
-    String queryStr;
+
+
+    Map<String, String> queryParams;
+
+    //삭제?id=2&no=1
     public Rq(String url) {
         this.url = url;
-        String[] urlBits = url.split("\\?",2);
+        String[] urlBits = url.split("\\?", 2);
         this.path = urlBits[0];
 
-        if(urlBits.length == 2){
-            this.queryStr = urlBits[1];
+        queryParams = new HashMap<>();
+
+        if (urlBits.length == 2) {
+            String queryStr = urlBits[1];
+
+            String[] paramBits = queryStr.split("&");
+
+            for (String paramBit : paramBits) {
+                String[] paramNameAndValue = paramBit.split("=");
+
+                if (paramNameAndValue.length == 1) {
+                    continue;
+                }
+                String paramName = paramNameAndValue[0].trim();
+                String paramValue = paramNameAndValue[1].trim();
+
+                queryParams.put(paramName, paramValue);
+            }
+
         }
     }
 
     public int getIntParam(String paramName, int defaultValue) {
-
-
-        if ( queryStr == null ) {
+        if (queryParams.containsKey(paramName) == false) {
             return defaultValue;
         }
 
-        String[] bits = queryStr.split("&");
+        String paramValue = queryParams.get(paramName);
+      if(paramValue.length() == 0){
+          return defaultValue;
+      }
 
-        for (String urlBit : bits) {
-
-            String[] paramNameAndValue = urlBit.split("=", 2);
-            String paramName_ = paramNameAndValue[0].trim();
-            String paramValue = paramNameAndValue[1].trim();
-
-
-
-            if (paramName.equals(paramName_)) {
-                return Integer.parseInt(paramValue);
-            }
-        }
-        return defaultValue;
     }
+
     public String getPath() {
-       return path;
+        return path;
     }
 }
